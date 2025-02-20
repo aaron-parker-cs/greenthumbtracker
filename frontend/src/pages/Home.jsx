1e2a1f64613d5e71f7c35cc8e4e33023f9a3529f
SlinkyGoat <adamustby@gmail.com>
2025-02-20 13:06:00 -0600

    Sidebar added
    Sidebar is created and working.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,15 @@
 import {
-  MDBContainer,
-  MDBCol,
-  MDBRow,
-  MDBCard,
-  MDBCardBody,
-  MDBCardText,
-  MDBCardTitle,
-} from "mdb-react-ui-kit";
-import {
-  Box,
-  Drawer,
+  Container,
   Button,
-  List,
-  ListItem,
-  ListItemText,
+  ListGroup,
+} from "react-bootstrap";
+import {
+  Drawer,
 } from '@mui/material';
-import { useState } from 'react';
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const Home = () => {
   const user = useSelector((state) => state.user);
@@ -28,31 +20,37 @@ const Home = () => {
   }
 
   const [open, setOpen] = useState(false);
-  const flowers = ["Rose", "Tulip", "Lily", "Daisy", "Sunflower", "Orchid", "Marigold", "Lavender", "Peony", "Chrysanthemum"];
+  const flowers = ["Rose", "Tulip", "Lily", "Daisy", "Sunflower", "Orchid", "Marigold", "Lavender", "Peony", "Chrysanthemum",
+    "Rose", "Tulip", "Lily", "Daisy", "Sunflower", "Orchid", "Marigold", "Lavender", "Peony", "Chrysanthemum"
+  ];
 
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
   };
 
+  // TODO: get user's plants from database and add them to ListGroup
+  // The flowers array is only temporary for visual purposes
   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-      <List>
+    <Container style={{width: "250px"}}>
+      <ListGroup>
         {flowers.map((flower, index) => (
-          <ListItem key={flower} disablePadding>
-            <ListItemText primary={flower} />
-          </ListItem>
-        ))};
-      </List>
-    </Box>
+          <ListGroup.Item>{`${index + 1} ${flower}`}</ListGroup.Item>
+        ))}
+      </ListGroup>
+    </Container>
   );
 
   return (
-    <MDBContainer>
-      <Button onClick={toggleDrawer(true)}>Open Drawer</Button>
+    <Container fluid className="my-2">
+      <Button variant="success" onClick={toggleDrawer(true)}>
+        {/* <i class="bi bi-chevron-double-right"></i> */}
+        {/* <i class="bi bi-justify"></i> */}
+        <i class="bi bi-list h4"></i>
+      </Button>
       <Drawer open={open} onClose={toggleDrawer(false)}>
         {DrawerList}
       </Drawer>
-    </MDBContainer>
+    </Container>
   );
 };
 
