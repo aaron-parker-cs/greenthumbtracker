fc04ce8da7c9e5c36a10fdc7f1a4fa97b43e25ab
Adam <adamustby@gmail.com>
2025-02-21 11:02:58 -0600

    Dashboard landing page (#6)
    * started side navbar for selecting plants to view
    
    Included a button which when clicked opens a side nav-bar which can be used to select specific plants which will allow the user to view data about the selected plant
    
    * Sidebar added
    
    Sidebar is created and working.
    
    * chart containers showing on sreen
    
    Containers to hold charts are now showing in a flex position so that the layout format stays the same (a grid-like pattern) even when containers are added/removed. Containers currently hold dummy data
    
    * graphs now show on dashboard
    
    Graphs with a header explaining the data alongside the graph with the data points can now be shown with the custom Chart Component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import {
-  MDBContainer,
-  MDBCol,
-  MDBRow,
-  MDBCard,
-  MDBCardBody,
-  MDBCardText,
-  MDBCardTitle,
-} from "mdb-react-ui-kit";
+  Container,
+  Row,
+  Col,
+  Button,
+  ListGroup,
+} from "react-bootstrap";
+import {
+  Drawer,
+} from '@mui/material';
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import Chart from '../components/common/Chart';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const Home = () => {
   const user = useSelector((state) => state.user);
@@ -18,26 +22,93 @@ const Home = () => {
     return <Navigate to="/login" />;
   }
 
+  const [open, setOpen] = useState(false); // used for side nav bar
+  const flowers = ["Dashboard", "Rose", "Tulip", "Lily", "Daisy", "Sunflower", "Orchid", "Marigold", "Lavender", "Peony", "Chrysanthemum",
+    "Rose", "Tulip", "Lily", "Daisy", "Sunflower", "Orchid", "Marigold", "Lavender", "Peony"
+  ];
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  // TODO: get user's plants from database and add them to ListGroup
+  // The flowers array is only temporary for visual purposes
+  const DrawerList = (
+    <Container style={{width: "250px"}}>
+      <ListGroup>
+        {flowers.map((flower, index) => (
+          <ListGroup.Item onClick={toggleDrawer(false)}> {/* TODO: change dashboard page to whatever was selected */}
+            {`${index + 1} ${flower}`}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </Container>
+  );
+
+  // Dummy info for visuals, replace with functioning GET data
+  const chartHeaders = ["Water Levels", "Sunlight", "Plant Height", "Soil Nutrients"];
+  const data = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+
   return (
-    <MDBContainer className="mt-5">
-      <MDBRow>
-        <MDBCol md="12">
-          <MDBCard>
-            <MDBCardBody>
-              <MDBCardTitle>Welcome, {user.username}!</MDBCardTitle>
-              <MDBCardText>
-                We're glad to have you back. Here's what's happening today:
-              </MDBCardText>
-              <ul>
-                <li>Check your plant's water levels</li>
-                <li>Review your garden's growth progress</li>
-                <li>Explore new gardening tips</li>
-              </ul>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+    <Container fluid className="pt-3"
+      style={{marginBottom: "82px", marginTop: "56px"}}> {/* bottom margin to keep from going under footer */}
+      <Button variant="success" onClick={toggleDrawer(true)}
+        style={{position: "fixed"}}>
+        <i class="bi bi-chevron-double-right"></i>
+      </Button>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+      <Container fluid className="d-inline-flex flex-wrap justify-content-around">
+        {chartHeaders.map((header, index) => (
+          <Chart header={header} data={data}/>
+        ))}
+      </Container>
+
+    </Container>
   );
 };
 
