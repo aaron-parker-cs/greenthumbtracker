2a21767d3dbc048e75457b0ec702f5ee65c9a794
SlinkyGoat <adamustby@gmail.com>
2025-03-27 16:13:42 -0500

    Current plant shown in navbar
    The current selected plant from the sidebar is shown in the center of the top navbar so user understands what the data is related to

--- frontend/src/pages/RecordsPage.tsx
+++ frontend/src/pages/RecordsPage.tsx
@@ -1,4 +1,9 @@
-
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { GrowthRecord } from "../models/growth";
+import { WaterRecord } from "../models/water";
+import { Unit } from "../models/unit";
+import { api } from "../redux/api";
 
 const RecordsPage = () => {
 
